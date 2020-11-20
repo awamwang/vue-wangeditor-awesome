e0b649a4b182e7ed4ff2cf78ae3b3fb7246d010a
Awam M Wang <wm.keepgoing@gmail.com>
2020-11-20 18:10:39 +0800

    docs: 添加examples

---
+++ examples/base/app.js
@@ -0,0 +1,17 @@
+Vue.use(VueWangEditor, /* { default global options } */)
+
+new Vue({
+  el: '#demo',
+
+  data() {
+    return {
+      content: `<p> default </p>` 
+    }
+  },
+
+  created: function () {},
+
+  watch: {},
+
+  methods: {},
+})
